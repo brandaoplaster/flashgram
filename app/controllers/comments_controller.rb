29e9e3ae3862ea720a0b16c3337a57f9deff35e9
brandaoplaster <brandaoplaster@gmail.com>
2020-12-29 21:40:54 -0300

    feat: adding controller comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment.post, notice: "Comentário enviado..."
+    else
+      flash.now[:alert] = @comment.errors.full_messages.to_sentense
+      render "posts/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
+end
